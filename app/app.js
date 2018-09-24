9d4b025ad182aa5228b33d7b53bb08098f0ac12b
VadimUA <puet12vadim@gmail.com>
2018-09-24 18:07:30 +0300

    After merge

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@
         ]).run(runBlock);
 
     /* @ngInject */
-    function runBlock($timeout, authSvc, $rootScope, $state, $transitions, pagesSvc) {
+    function runBlock($timeout, authDataSvc, $rootScope, $state, $transitions, pagesSvc) {
 
         // $state.go('app.start-page');
         // $timeout(function(){
