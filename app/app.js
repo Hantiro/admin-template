842505d1692f8e8239eafe55b6a759cba74ce677
Hantiro <caucho228@gmail.com>
2018-10-22 18:26:19 +0300

    before merge

--- app/app.js
+++ app/app.js
@@ -16,7 +16,6 @@
         // });
         //
 
-
         $transitions.onBefore({}, function(transition) {
             // check if the state should be auth
             if ( transition.to().data && transition.to().data.auth && !authDataSvc.isLogined()) {
