871fedd22d966c04deb03fcb4ffed8bb95759ecd
VadimUA <puet12vadim@gmail.com>
2019-01-03 16:07:38 +0200

    Add payment functionality

--- app/directives/upgrade-full/upgrade-full.ctrl.js
+++ app/directives/upgrade-full/upgrade-full.ctrl.js
@@ -6,11 +6,21 @@
         .controller('UpgradeFullCtrl', UpgradeFullCtrl);
 
     /* @ngInject */
-    function UpgradeFullCtrl($scope) {
+    function UpgradeFullCtrl($scope, paymentSvc, authDataSvc) {
         var vm = this;
+        vm.link = '';
+        vm.isShow = true;
 
         init();
         function init(){
+            getLink();
+            vm.isShow = Boolean( authDataSvc.getUser() && (!authDataSvc.getUser().last_payment || !authDataSvc.getUser().is_payed) );
+        }
+
+        function getLink(){
+            paymentSvc.getUrl().then(function(res){
+                vm.link =  res.data;
+            });
         }
 
     }
